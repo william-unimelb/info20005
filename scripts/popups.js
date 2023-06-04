b2180ba421c69d54fc572cd2870ba92d0aed368d
William Du <dwz99999@gmail.com>
2023-06-05 09:42:30 +1000

    show/hide different popups

---
+++ scripts/popups.js
@@ -0,0 +1,39 @@
+function showProduct() {
+    let overlay = document.getElementById("product-overlay");
+    overlay.classList.remove("overlay--hidden");
+}
+
+function hideProduct() {
+    let overlay = document.getElementById("product-overlay");
+    overlay.classList.add("overlay--hidden");
+}
+
+function showCart() {
+    let overlay = document.getElementById("cart-overlay");
+    overlay.classList.remove("overlay--hidden");
+}
+
+function hideCart() {
+    let overlay = document.getElementById("cart-overlay");
+    overlay.classList.add("overlay--hidden");
+}
+
+function showSearch() {
+    let overlay = document.getElementById("search-overlay");
+    overlay.classList.remove("overlay--hidden");
+}
+
+function hideSearch() {
+    let overlay = document.getElementById("search-overlay");
+    overlay.classList.add("overlay--hidden");
+}
+
+function showMenu() {
+    let overlay = document.getElementById("menu-overlay");
+    overlay.classList.remove("overlay--hidden");
+}
+
+function hideMenu() {
+    let overlay = document.getElementById("menu-overlay");
+    overlay.classList.add("overlay--hidden");
+}
